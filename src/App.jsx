4b5917a6beab31bd0b52e819fd700be68cc61deb
Menskyi Pavlo <s2400327@BC-FVFLD1FX1WG7s-MacBook-Air.local>
2024-11-13 14:02:44 +0200

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,64 @@
+import { useState } from 'react'
+import { PropTypes } from 'prop-types'
+
+const History = (props) => {
+  if (props.allClicks.length === 0) {
+    return (
+      <div>
+        the app is used by pressing the buttons
+      </div>
+    )
+  }
+  return (
+    <div>
+      button press history: {props.allClicks.join(' ')}
+    </div>
+  )
+}
+
+History.propTypes = {
+  allClicks: PropTypes.array,
+}
+
+const Button = ({ handleClick, text }) => (
+  <button onClick={handleClick}>
+    {text}
+  </button>
+)
+
+Button.propTypes = {
+  handleClick: PropTypes.func,
+  text: PropTypes.string
+}
+
+const App = () => {
+  const [left, setLeft] = useState(0)
+  const [right, setRight] = useState(0)
+  const [allClicks, setAll] = useState(0)
+   
+  const handleLeftClick = () => {
+    setAll(allClicks.concat('L'))
+    const updatedLeft = left + 1
+    setLeft(updatedLeft)
+  }
+
+  const handleRightClick = () => {
+    setAll(allClicks.concat('R'))
+    const updatedRight = right + 1
+    setRight(updatedRight)
+  }
+
+  return (
+    <div>
+      <div>
+        {left}
+        <Button handleClick={handleLeftClick} text='left' />
+        <Button handleClick={handleRightClick} text='right' />
+        {right}
+        <History allClicks={allClicks} />
+      </div>
+    </div>
+  )
+}
+
+export default App
