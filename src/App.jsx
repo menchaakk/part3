f10417892bad7bb435d916099b9eb06aaaf5470f
menchaakk <s2400327@BC-FVFLD1FX1WG7s-MacBook-Air.local>
2025-01-23 13:33:29 +0200

    first commit

--- src/App.jsx
+++ src/App.jsx
@@ -34,7 +34,7 @@ Button.propTypes = {
 const App = () => {
   const [left, setLeft] = useState(0)
   const [right, setRight] = useState(0)
-  const [allClicks, setAll] = useState(0)
+  const [allClicks, setAll] = useState([])
    
   const handleLeftClick = () => {
     setAll(allClicks.concat('L'))
